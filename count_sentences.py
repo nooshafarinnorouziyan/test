c2054dba9a3d9aea963186e21e190da58f9c9f8a
nooshafarinnorouziyan <140313181+nooshafarinnorouziyan@users.noreply.github.com>
2023-08-16 13:04:41 -0700

    Update count_sentences.py

--- count_sentences.py
+++ count_sentences.py
@@ -1,3 +1,7 @@
 def count_words(input_string):
     words = input_string.split()
     return len(words)
+
+input_string = "This is a sample input string"
+word_count = count_words(input_string)
+print("Number of words in input string:", word_count)
