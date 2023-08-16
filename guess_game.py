4aede7111b1c6ba7f9ab140c78693d21388e7ad8
nooshafarinnorouziyan <140313181+nooshafarinnorouziyan@users.noreply.github.com>
2023-08-16 13:06:59 -0700

    Update guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,22 +1,24 @@
+
 import random
 print("this is a guessing game")
-your_guess = int(input ("!!!!!enter your guess!!!!!:"))
-random_number= random.randint(0,20)
 
+random_number= random.randint(1,20)
 count=1
-while 1 :
 
- if your_guess==random_number:
-      print("*****you are win*****") 
-      break 
+while count <= 5 :
+    your_guess = int(input("!!!!!enter your guess!!!!!:"))
+
+    if your_guess == random_number:
+        print("*****you win*****") 
+        break 
     
- elif your_guess > random_number:
+    elif your_guess < random_number:
         print("<<<<your guess is low>>>>")
-count+=1
-
-if your_guess < random_number:
+        
+    elif your_guess > random_number:
         print("<<<<your guess is much>>>>")
-count+=1
+        
+    count += 1
 
-  
-    
+if count > 5:
+    print("Sorry, you have exhausted your 5 guesses. The random number was:", random_number)
