c00703305e271e2692b105f522ac9f827a973c2c
nooshafarinnorouziyan <140313181+nooshafarinnorouziyan@users.noreply.github.com>
2023-08-16 13:13:23 -0700

    Update random.py

--- random.py
+++ random.py
@@ -1,10 +1,13 @@
 import random 
-print ("///the random list///")
-number_ed=int(input("how many number are in the list???"))
-num_list =[]
-while len (num_list) < number_ed:
-    number = random.randint(1,number_ed+10)
-if number not in num_list:
-    num_list.append(number)
-    result = num_list
-    print("your list is complete")
+
+print("///the random list///")
+number_ed = int(input("how many number are in the list???"))
+num_list = []
+
+while len(num_list) < number_ed:
+    number = random.randint(1, number_ed+10)
+    if number not in num_list:
+        num_list.append(number)
+
+print("Your list is complete!")
+print(num_list)
