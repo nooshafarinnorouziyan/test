128c55e318a0553ade5020ef2f5617058e2918bc
nooshafarinnorouziyan <140313181+nooshafarinnorouziyan@users.noreply.github.com>
2023-08-16 13:03:21 -0700

    Update chees.py

--- chees.py
+++ chees.py
@@ -1,9 +1,13 @@
 import math
-def chees(m,n):
-    for i in range (m):
-     for j in range (n):
-        if(i+j)%2==0:
-           print("*")
-        else :
-           print("#")
-           print()
+
+def chees(m, n):
+    for i in range(m):
+        for j in range(n):
+            if (i+j) % 2 == 0:
+                print("*", end="")
+            else:
+                print("#", end="")
+        print()
+
+# Example usage
+chees(3, 5)
